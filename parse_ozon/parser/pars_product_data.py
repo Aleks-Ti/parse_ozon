779bc7a5d79ecfd4f5802b5433c844a9142e0273
Aleks-Ti <mti61ro@yandex.ru>
2023-12-19 20:08:47 +0300

    tls_client

--- parse_ozon/parser/pars_product_data.py
+++ parse_ozon/parser/pars_product_data.py
@@ -1,12 +1,14 @@
 # https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=/searchSuggestions/?text=&url=/seller/proffi-1/products/
 import tls_client
+from pprint import pprint
 
-url = 'https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=/seller/proffi-1/products/?page=2' # ?page=1
+url = 'https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=/seller/proffi-1/products/' # ?page=1
 
 session = tls_client.Session(
     client_identifier="chrome112", random_tls_extension_order=True
 )
 
 response = session.get(url)
+response.status_code
 d = response.json()
-print(d)
+pprint(d)
